3b4afd438a58d1a4859c762f9437de5c170d10dd
ankitghoshthecreator <baruipara01@gmail.com>
2024-08-31 23:21:15 +0530

    text area added where we write

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
     JFrame window;
+    JTextArea textArea;
     public static  void main(String[] args){
 
 
@@ -9,12 +11,24 @@ public class GUI {
 
     }
     public GUI(){
+        createWindow();
+        createTextArea();
+
+
+        window.setVisible(true);
 
     }
 
-    public void createWindow(){
-       window = new JFrame("Notepad");
+    public void createWindow(){ // window creation
+       window = new JFrame("VAAYU-pad");
        window.setSize(900,700);
+       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public void createTextArea(){
+        textArea=new JTextArea();
+        window.add(textArea);
     }
 
 }
