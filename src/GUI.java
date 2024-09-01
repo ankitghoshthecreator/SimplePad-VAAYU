734d1cdf1328b6190c5adc785d60cbe0d980b0a4
ankitghoshthecreator <baruipara01@gmail.com>
2024-09-01 15:28:41 +0530

    menue bar added -> File, Edit, Format, Color and after that we added dropdown list of File

--- src/GUI.java
+++ src/GUI.java
@@ -6,6 +6,9 @@ public class GUI {
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor ;
+    JMenuItem iNew, iSave, iSaveAs,iExit,iOpen;
     public static  void main(String[] args){
 
 
@@ -15,6 +18,8 @@ public class GUI {
     public GUI(){
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
 
 
         window.setVisible(true);
@@ -30,12 +35,49 @@ public class GUI {
 
     public void createTextArea(){
         textArea=new JTextArea();
-        scrollPane=new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane=new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);// intitalize scroll bar class
         // in the above line we gave the textAres in it so it will be in it included and we do not need this line window.add(textArea);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
 
         window.add(scrollPane); // and we add the scrollPane to our window
+    }
+
+    public void createMenuBar(){// menue bar added in our pplication
+        menuBar=new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile=new JMenu("File");
+        menuBar.add(menuFile);
+
+
+        menuFormat=new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColor=new JMenu("Color");
+        menuBar.add(menuColor);
+
+        menuEdit=new JMenu("Edit");
+        menuBar.add(menuEdit);
+        //menu bar items done
+    }
+
+    public void createFileMenu(){
+        iNew=new JMenuItem("New");//create objects
+        menuFile.add(iNew);
+
+        iOpen=new JMenuItem("Open");
+        menuFile.add(iOpen);
+
+        iSave=new JMenuItem("Save");//create objects
+        menuFile.add(iSave);
+
+        iSaveAs=new JMenuItem("Save As");//create objects
+        menuFile.add(iSaveAs);
+
+        iExit=new JMenuItem("Exit");//create objects
+        menuFile.add(iExit);
+
 
     }
 
