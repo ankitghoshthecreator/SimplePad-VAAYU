b795a1a1edde54c4bf34e39c7d1346237cfa74db
ankitghoshthecreator <baruipara01@gmail.com>
2024-09-03 20:02:52 +0530

    file loading completed we can open different files

--- src/Function_File.java
+++ src/Function_File.java
@@ -1,8 +1,13 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Function_File {
 
     GUI gui;
+
+    String fileName;
+    String fileAddress;
     public Function_File(GUI gui){
         this.gui=gui; // for accessing the members from the GUI class
     }
@@ -14,5 +19,22 @@ public class Function_File {
         FileDialog fd = new FileDialog(gui.window ,"Open", FileDialog.LOAD); // 1. open window 2. what written in title top of window 3. what written
         fd.setVisible(true);
 
+        if(fd.getFile()!=null){
+            fileName= fd.getFile();
+            fileAddress= fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+        try{
+            BufferedReader br= new BufferedReader(new FileReader(fileAddress + fileName));// we need the address and the name to read the file or to load the file
+            gui.textArea.setText("");
+            String line = null;
+
+            while ((line = br.readLine())!=null){
+                gui.textArea.append(line + "\n");
+            }
+            br.close();
+        }catch (Exception e){
+            System.out.println("File not opened");
+        }
     }
 }
